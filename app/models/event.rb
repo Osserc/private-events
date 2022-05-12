9415a54d1fb5d7fa86c5f525c1dae657a37525ef
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-12 22:16:00 +0200

    Add styling to flash messages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
+
+    validates :title, :location, :date, :description, presence: true
     belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, :username, presence: true
+
   has_many :events
 end
