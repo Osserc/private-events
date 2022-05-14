86589c09df9e1a3bf92afded4aa9ad2d4985060f
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-14 18:26:22 +0200

    Add attended events to users

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,7 @@ class Event < ApplicationRecord
     has_many :participations, foreign_key: :attended_event_id
     has_many :attendees, through: :participations
 
+    scope :current, -> { where("date > ?", Time.now) }
+    scope :past, -> { where("date < ?", Time.now) }
+
 end
